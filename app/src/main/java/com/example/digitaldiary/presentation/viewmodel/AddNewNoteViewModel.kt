4cd94777e608efd144fbd31f1edd05729de9aec6
PawelManczak <pawel14522@gmail.com>
2024-05-23 17:25:47 +0200

    minor fix

--- app/src/main/java/com/example/digitaldiary/presentation/viewmodel/AddNewNoteViewModel.kt
+++ app/src/main/java/com/example/digitaldiary/presentation/viewmodel/AddNewNoteViewModel.kt
@@ -2,9 +2,10 @@ package com.example.digitaldiary.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class AddNewNoteViewModel constructor() : ViewModel() {
+class AddNewNoteViewModel @Inject constructor() : ViewModel() {
 
 
 }
