76ebb85720f41f450028aa966171786cd7979ff8
PawelManczak <pawel14522@gmail.com>
2024-05-26 22:14:41 +0200

    ui changes

--- app/src/main/java/com/example/digitaldiary/presentation/screen/NoteDetailsScreen.kt
+++ app/src/main/java/com/example/digitaldiary/presentation/screen/NoteDetailsScreen.kt
@@ -2,20 +2,23 @@ package com.example.digitaldiary.presentation.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
-import com.example.digitaldiary.data.NotePreview
 import com.example.digitaldiary.presentation.components.FullScreenLoadingIndicator
 import com.example.digitaldiary.presentation.viewmodel.NoteDetailsState
 import com.example.digitaldiary.presentation.viewmodel.NoteDetailsViewModel
@@ -27,10 +30,9 @@ fun NoteDetailsScreen(id: String) {
 
     val vm = hiltViewModel<NoteDetailsViewModel>()
 
-    if(vm.state.value.isLoading){
+    if (vm.state.value.isLoading) {
         FullScreenLoadingIndicator()
-    }
-    else{
+    } else {
         NoteDetailsContent(vm.state.value)
 
 
@@ -41,20 +43,33 @@ fun NoteDetailsScreen(id: String) {
 
 @Composable
 fun NoteDetailsContent(state: NoteDetailsState) {
-    Column(Modifier.fillMaxSize().padding(16.dp)) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.Start
+    ) {
         Text("Note Details", style = MaterialTheme.typography.titleLarge)
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(state.note!!.title, style = MaterialTheme.typography.titleMedium)
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(state.note.content, style = MaterialTheme.typography.bodyMedium)
 
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row{
+            Text(text = state.note!!.title, style = MaterialTheme.typography.titleMedium, textAlign = TextAlign.Start, modifier = Modifier.weight(1f))
+            Text(text = state.note.city, style = MaterialTheme.typography.bodyMedium, textAlign = TextAlign.End)
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(state.note!!.content, style = MaterialTheme.typography.bodyMedium)
+
+        Spacer(modifier = Modifier.height(16.dp))
         state.photoUrl?.let { uri ->
             Image(
                 painter = rememberAsyncImagePainter(uri),
                 contentDescription = "Note Photo",
                 modifier = Modifier
-                    .fillMaxWidth()
                     .height(200.dp)
+                    .shadow(15.dp)
             )
         }
     }
